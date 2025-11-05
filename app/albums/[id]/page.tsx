28c94861d44861014f90f00058e743e3e00467e3
michael-daniel <mdaniel.info@orange.fr>
2025-11-05 12:30:15 +0100

    maj1230

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -3,18 +3,22 @@ import { prisma } from "@/app/page";
 import TracksList from "./TracksList"
 
 export default async function AlbumPage({ params }: { params: { id: string } }) {
-    const {id} = await params;
-  const album = await prisma.album.findUnique({
-    where: { id: Number(id) },
-  });
+    const { id } = await params;
+    const album = await prisma.album.findUnique({
+        where: { id: Number(id) },
+    });
 
-  if (!album) return <div>Album introuvable</div>;
+    if (!album) return <div>Album introuvable</div>;
 
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">{album.title}</h1>
-      <p className="text-gray-500">{album.artist}</p>
-      <TracksList albumId={album.id} />
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">{album.title}</h1>
+            <p className="text-gray-500">{album.artist}</p>
+            <TracksList albumId={album.id} />
+        </div>
+    );
 }
+
+// SSR => on transmet les données récupérées par Prisma en utilisant un composant enfant (TrackList), et en mettant l'id de l'Album dans les props.
+
+// L'enfant peut alors faire un fetch sur l'API en utilisant l'albumId => useEffect => useState => affichage dynamique de la liste des tracks
