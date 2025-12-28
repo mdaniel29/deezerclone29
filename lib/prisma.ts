91e2604c8d70260a17b9b2ceb10b8756cba43b02
michael-daniel <mdaniel.info@orange.fr>
2025-12-29 00:07:15 +0100

    MAJ HostArmada

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,12 @@ const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
+const adapter = new PrismaMariaDb({
+  host: "178.162.233.217",
+  port: 3306,
+  connectionLimit: 20
+})
+
 const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
