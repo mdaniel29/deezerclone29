33a3131e5befc4f7e42b17c3fe393da1a01d2894
michael-daniel <mdaniel.info@orange.fr>
2026-01-28 16:19:04 +0100

    m1618

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,24 @@
 import { PrismaClient } from "../app/generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
+//import pg from "pg"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-
-const connectionString = process.env.DATABASE_URL
-
-const pool = new pg.Pool({ connectionString })
-const adapter = new PrismaPg(pool)
+//const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL_SUPA
+//const pool = new pg.Pool({ connectionString })
+//const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg({
+	connectionString, // chaine de connexion (direct connect 5432, NOT pool)
+})
 
 const prisma = globalForPrisma.prisma || new PrismaClient(
     {
         adapter,
-         errorFormat: "pretty",
+        errorFormat: "pretty",
     }
 )
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
 export default prisma
-
 
